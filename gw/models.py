461f63aee0d1c01584cb47eec5bc0c98c5b2ff98
YoonDuck <dkdlel616@gmail.com>
2025-02-12 09:58:01 +0900

    250212 Recommend function

--- gw/models.py
+++ gw/models.py
@@ -9,8 +9,9 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField(default=now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     modify_date = models.DateTimeField(null=True, blank=True) # 수정 일시 추가, null 허용, 값 없어도 허용
+    voter = models.ManyToManyField(User, related_name='voter_question')
     
     def __str__(self):
         return self.subject
@@ -21,5 +22,10 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField(default=now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     modify_date = models.DateTimeField(null=True, blank=True) # 수정 일시 추가, null 허용, 값 없어도 허용
+    voter = models.ManyToManyField(User, related_name='voter_answer')
+    
+    # author와 voter가 User 모델과 연결되어 있어, User.question_set으로 Question 데이터에 접근하려고 할 때, 구분이 필요
+    # related_name 인수를 추가하여 해결
+    # 특정 사용자가 작성한 질문 : some_user.author_question.all(), 특정 사용자가 추천한 질문 : some_user.voter_question.all()
